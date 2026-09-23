6c38fc9060aa7da9a933d43340c6803f822dbd7a
Valyushka2023 <vvpto@ukr.net>
2026-09-23 20:53:29 +0300

    coffee-comfort
    ch205323092026

--- src/components/Modal/ModalCardMenu/ModalCardMenu.jsx
+++ src/components/Modal/ModalCardMenu/ModalCardMenu.jsx
@@ -59,14 +59,14 @@ const ModalCardMenu = ({ item, onClose, formatPrice }) => {
         onClick={e => e.stopPropagation()}
         style={{ display: isImageLoaded ? 'flex' : 'none' }}
       >
-        <button
+        {/* <button
           className={css['close-icon']}
           onClick={onClose}
           type="button"
           aria-label="Close"
         >
           &times;
-        </button>
+        </button> */}
 
         <div className={css['modal-body']}>
           {/* Прихована картинка, яка тригерить стейт при повній готовності */}
